d93a796c7f6e2ca4460e14067a1c04d718f09d63
Cyrus Huang <cyrus.kernel@gmail.com>
2025-04-17 10:39:57 +0800

    split rknn pc code and board code

--- models/iris_model/runtime_rknn.py
+++ models/iris_model/runtime_rknn.py
@@ -1,32 +1,84 @@
 import os
 import sys
+import platform
+import numpy as np
 from rknn.api import RKNN
+
+def print_result(sample, result):
+    print('Sample:', sample)
+    print('Prediction:', result)
+    print('Result:', np.argmax(result))
+
+def runtime_pc(onnx_path, rknn_path, sample):
+
+    rknn = RKNN()
+
+    rknn.config(target_platform='rk3588') # Define input shape: "dynamic_input=[[[1, 4]]]"
+    
+    print('Load onnx model:', onnx_path)
+    ret = rknn.load_onnx(onnx_path, inputs=['keras_tensor'], input_size_list=[[1, 4]])
+    if ret:
+        exit('ERROR: Load onnx failed.')
+
+    ret = rknn.build(do_quantization=False)
+    if ret:
+        exit('ERROR: Build rknn failed.')
+
+    ret = rknn.export_rknn(rknn_path)
+    if ret:
+        exit('ERROR: Export rknn failed.')
+    print('Save rknn model:', rknn_path)
+
+    print('Init simulation runtime...')
+    ret = rknn.init_runtime(target=None)
+    if ret:
+        exit('ERROR: Runtime init failed.')
+
+    result = rknn.inference(inputs=[sample])
+    
+    print_result(sample, result[0])
+
+    rknn.release()
+
+def runtime_board(rknn_path, sample):
+
+    rknn = RKNN()
+
+    print('Load rknn model:', rknn_path)
+    ret = rknn.load_rknn(rknn_path)
+    if ret:
+        exit('ERROR: Load rknn failed.')
+        
+    print('Init rknn runtime.')
+    ret = rknn.init_runtime(target='rk3588')
+    if ret:
+        exit('ERROR: Runtime init failed.')
+
+    result = rknn.inference(inputs=[sample])
+    
+    print_result(sample, result[0])
+
+    rknn.release()
 
 if __name__ == '__main__':
 
-    build_model = len(sys.argv) >=2 and sys.argv[1] == '1'
-
     onnx_path = './iris_model.onnx'
     rknn_path = './iris_model.rknn'
+    
+    sample = np.array([[5.1, 3.5, 1.4, 0.2]], dtype=np.float32)
+    
+    arch = platform.machine()
 
-    rknn = RKNN()
-    rknn.config(target_platform='rk3588') # Define input shape: "dynamic_input=[[[1, 4]]]"
+    if arch == 'x86_64':
 
-    if os.path.exists(rknn_path) and not build_model:
-        ret = rknn.load_rknn(rknn_path)
-        if ret:
-            exit('ERROR: Load rknn failed.')
+        from rknn.api import RKNN
+        runtime_pc(onnx_path, rknn_path, sample)
+
+    elif arch == 'aarch64':
+
+        from rknnlite.api import RKNNLite as RKNN
+        runtime_board(rknn_path, sample)
+
     else:
-        ret = rknn.load_onnx(onnx_path, inputs=['keras_tensor'], input_size_list=[[1, 4]])
-        if ret:
-            exit('ERROR: Load onnx failed.')
+        exit('ERROR: Unsupport platform.')
 
-        ret = rknn.build(do_quantization=False)
-        if ret:
-            exit('ERROR: Build rknn failed.')
-
-        ret = rknn.export_rknn(rknn_path)
-        if ret:
-            exit('ERROR: Export rknn failed.')
-
-    rknn.release()
