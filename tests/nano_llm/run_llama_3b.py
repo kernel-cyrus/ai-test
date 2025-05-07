2cd82f204fe2b92b79ed8a79501fd88a2d31f08a
Cyrus Huang <archman@126.com>
2025-05-07 14:25:36 +0800

    add prompt

--- tests/nano_llm/run_llama_3b.py
+++ tests/nano_llm/run_llama_3b.py
@@ -11,7 +11,14 @@ model = NanoLLM.from_pretrained(
        quantization='q4f16_ft'                              # q4f16_ft, q4f16_1, q8f16_0 for MLC, or path to AWQ weights
 )
 
-response = model.generate("Once upon a time,", max_new_tokens=128)
+while True:
 
-for token in response:
-       print(token, end='', flush=True)
+    prompt = input("prompt: ")
+
+    if prompt == 'exit':
+        break
+
+    response = model.generate(prompt, max_new_tokens=1024)
+
+    for token in response:
+        print(token, end='', flush=True)
